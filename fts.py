49fc0d33755e4178f09f337a71917ccbd499557d
Giordano <giordanoscarso@gmail.com>
2020-08-19 13:31:51 +0200

    Updated creation of uploads folder and changed no file error returning value

--- fts.py
+++ fts.py
@@ -18,19 +18,17 @@ def allowed_file(filename):
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if not os.path.exists(os.path.dirname(UPLOAD_FOLDER)):
-        os.makedirs(os.path.dirname(UPLOAD_FOLDER));
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER);
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+            return 'No file part'
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+            return 'No selected file'
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
