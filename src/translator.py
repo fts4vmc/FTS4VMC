6853c5f0aeba2016e57214a164688589a682703c
Michele V <michele.valfre@e>
2020-08-28 15:39:06 +0200

    First working version of the translator from '.dot' to VMC accepted format.

--- src/translator.py
+++ src/translator.py
@@ -4,76 +4,55 @@
 
 import sys
 import pydot
+import traceback
 
-import src.analyser
+import analyser
 #From analyser we use:
 #   function load_dot(path)
-    
-class Translator:
-    __fts
-    __mtsv
 
-    def load_model(path):
+class Translator:
+    __slots__= '__fts', '__mtsv'
+
+    def load_model(self,path):
         #path := path to DOT file
         try:
-            __fts = src.load_dot(path)
+            self.__fts = analyser.load_dot(open(path,"r"))
         except Exception as e:
-            print(str(e))
+            traceback.print_exc()
             sys.exit(0)
 
     def translate(self):
-        if __fts == None:
+        if self.__fts == None:
             raise Exception('No model loaded!')
             return None
         self.__mtsv = ''
-        
-        #States
-        #The set of states of the new MTSv is obtained from
-        #the FTS's by adding a fresh state to it.
-        q = self.__fts._states
-        sink_state = State('sink state')
-        q.add(sink_state)
-        
-        #Actions, May/Must transitions, Variability constraints
-        #Those sets are obtained from the FTS as follows:
-        sigma = {}
-        delta_diamond = {}
-        var_constr = {}
+        start = 'SYS = C'
+        adj = {}#Dictionary used to register the adjacencies  
         for t in self.__fts._transitions:
-            #Actions
-            #For each transition (q,a,phi,q') in the FTS's transition relation
-            #where " q,q' " are states, " a " is a label and " phi " is a feature 
-            #symbol, add (a,phi) to the set
-            sigma.add((t._label,t._constraint))
-            #For each label " a " which does not appear in a transition, add " a "
-            #to the set
-            sigma.add(t._label)
-            #For each feature symbol " phi ", add " phi " to the set
-            sigma.add(t._constraint)
-            
-            #May transitions - part one
-            #For each transition (q,a,phi,q') in the FTS's transition relation,
-            #add (q,(a,phi),q') to the MTSv's may transition relation
-            delta_diamond.add((t._in,(t._label,t._constraint)t._out))
-            #not done yet: see the comment after the for
+            if(t._in._id == self.__fts._initial):
+                start += str(t._in._id)
+            id = str(t._in._id)
+            if id not in adj:
+                tmp = 'C' + id + ' = ' + str(t._label) + '(may).' + 'C' + str(t._out._id)
+                adj.update([(id,tmp)])
+            else:
+                adj[id] += ' + ' + str(t._label) + '(may).' + 'C' + str(t._out._id)
+        for a in adj:
+            self.__mtsv += adj[a] + '\n'
+        self.__mtsv += '\n' + start + '\n\nConstraints{ LIVE }'
 
-            #The Must transitions set is left empty
+    def get_mtsv(self):
+        if self.__mtsv != None:
+            return self.__mtsv
+        else: return 'No model loaded'
+###########################FOR TESTING PURPOSES ONLY#################
+import sys
 
-            #Variability constraints - part one
-            #For each (q,a,phi,q') in the FTS's transition relation, add 
-            # (a,phi) iff phi to the MTSv's variability constraints
-            var_constr.add('(' + str(t._label) + ',' + str(t._constraint)  + ')' + ' IFF ' + str(t._constraint))
-            
+def main():
+    t = Translator()
+    t.load_model('../../VMC/FTS-vendingnew.dot')
+    t.translate()
+    print(t.get_mtsv())
 
-        #May transitions - part two
-        #Let sigma_diff be the difference between the set of states of the
-        #MTSv(sigma) and the set of states of the FTS, let q_start be the
-        #initial state of the FTS, let s be the previously created sink state.
-        #Then, for each symbol " sym " in sigma_diff, add (q_start,sym,s) to 
-        #the MTSv's may transition relation
-        with sigma - self.__fts._states as sigma_diff:
-            for sym in sigma_diff:
-                delta_diamod.add(self.__fts._initial,sym,sink_state)
-
-        #Variability constraints - part two
-        analyser.z3_analyse_full()
+if __name__ == '__main__':
+    main()
