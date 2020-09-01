70de6379c2e5a2339c56b6096119df54020f1208
Giordano Scarso <giordanoscarso@gmail.com>
2020-09-01 18:51:57 +0200

    Merged fts.py from realtime_output branch

--- src/fts.py
+++ src/fts.py
@@ -1,7 +1,10 @@
 import os
-import src.analyser
-import subprocess
-from flask import send_from_directory
+import sys
+import time
+import analyser
+from process_manager import ProcessManager
+import multiprocessing
+from flask import make_response, session
 
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
@@ -9,15 +12,82 @@ from werkzeug.utils import secure_filename
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'dot'}
 
+process = {}
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = b'\xb1\xa8\xc0W\x0c\xb3M\xd6\xa0\xf4\xabSmz=\x83'
+
+def full_analysis_worker(fts_file, out_file):
+    fts_source = open(fts_file, 'r')
+    sys.stdout = open(out_file, 'w')
+    fts = analyser.load_dot(fts_source)
+    analyser.z3_analyse_full(fts)
+    fts.report()
+    sys.stdout.close()
+
+def hdead_analysis_worker(fts_file, out_file):
+    fts_source = open(fts_file, 'r')
+    sys.stdout = open(out_file, 'w')
+    fts = analyser.load_dot(fts_source)
+    analyser.z3_analyse_hdead(fts)
+    fts.report()
+    sys.stdout.close()
+
+def check_session():
+    pm = ProcessManager.get_instance()
+    if 'timeout' in session and session['timeout'] is not None and pm.process_exists(session['id']):
+        if session['timeout'] > time.time():
+            return True
+    return False
+
+def new_session():
+    now = time.time()
+    session['position'] = 0
+    session['timeout'] = now+60*60
+    session['output'] = '/tmp/' + str(now) + '-output'
+
+def close_session():
+    pm = ProcessManager.get_instance()
+    session.pop('timeout', None)
+    session.pop('position', None)
+    if 'id' in session and session['id']:
+        pm.end_process(session['id'])
+    if 'output' in session:
+        try:
+            os.remove(session['output'])
+        except FileNotFoundError:
+            pass
+    session.pop('id', None)
+    session.pop('output', None)
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/yield')
+def get_output():
+    if not check_session():
+        return make_response(('\nSession timed-out', "200"))
+    pm = ProcessManager.get_instance()
+    if not pm.process_exists(session['id']):
+        return make_response(('', "200"))
+    else:
+        with open(session['output']) as out:
+            if pm.is_alive(session['id']):
+                out.seek(session['position'])
+                result = out.read(4096)
+                session['position'] = out.tell()
+                return make_response((result, "206"))
+            else:
+                out.seek(session['position'])
+                result = out.read()
+                close_session()
+                return make_response((result, "200"))
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    new_session()
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER);
     if request.method == 'POST':
@@ -42,24 +112,39 @@ def index():
 
 @app.route('/full_analysis', methods=['POST'])
 def full_analyser():
+    pm = ProcessManager.get_instance()
+    close_session()
+    new_session()
     filename = secure_filename(request.form['name'])
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     if os.path.isfile(file_path):
-        result = subprocess.getoutput('python3 src/analyser.py '+ file_path)
-        return result
+        thread = multiprocessing.Process(target=full_analysis_worker,
+                args=[file_path, session['output']])
+        session['id'] = str(thread.name)
+        pm.add_process(session['id'], thread)
+        pm.start_process(session['id'])
+        return "Processing data..."
     return 'File not found'
 
 @app.route('/hdead_analysis', methods=['POST'])
 def hdead_analyser():
+    pm = ProcessManager.get_instance()
+    close_session()
+    new_session()
     filename = secure_filename(request.form['name'])
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     if os.path.isfile(file_path):
-        result = subprocess.getoutput('python3 src/analyser.py --hdead '+ file_path)
-        return result
+        thread = multiprocessing.Process(target=hdead_analysis_worker,
+                args=[file_path, session['output']])
+        session['id'] = str(thread.name)
+        pm.add_process(key=session['id'], process=thread)
+        pm.start_process(session['id'])
+        return "Processing data..."
     return 'File not found'
 
 @app.route('/delete_model', methods=['POST'])
 def delete_model():
+    close_session()
     filename = secure_filename(request.form['name'])
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     try:
@@ -69,3 +154,8 @@ def delete_model():
         return "An error occured while deleting the file model"
     else:
         return "Model file deleted"
+
+@app.route('/stop', methods=['POST'])
+def stop_process():
+    close_session()
+    return 'Stopped process'
