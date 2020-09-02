40a4ed691dae119d5bd06edf8bd0133c53bdc695
Michele V <michele.valfre@e>
2020-09-02 16:44:46 +0200

    Added buttons/functions to handle the VMC section of the GUI

--- src/static/script.js
+++ src/static/script.js
@@ -4,6 +4,8 @@ $(function(){
     $("aside").on("click", "#hdead", {url: '/hdead_analysis', success:timed_update_textarea}, command);
     $("aside").on("click", "#delete", {url: '/delete_model', success:update_textarea}, command);
     $("aside").on("click", "#stop", {url: '/stop', success:update_textarea}, command);
+    $("aside").on("click", "#load_prop", upload_prop_file);
+    $("aside").on("click", "#run_vmc", {url: '/vmc', success:update_textarea} ,command_vmc)
 }); 
 
 //Updates the main's textarea with the response value.
@@ -57,6 +59,28 @@ function command(event)
     }
 }
 
+function command_vmc(event)
+{
+    if($("#fts")[0].files[0]) {
+        if($("#vmc")[0].files[0]){
+            $.ajax({
+                url: event.data.url,
+                data: {name: $("#fts")[0].files[0].name,
+                       prop_name: $("#vmc")[0].files[0].name},
+                type: 'POST',
+                success: event.data.success,
+                beforeSend: function(response) {
+                    $("main > textarea").text("Processing data...");
+                }
+            });
+        } else {
+            $("main > textarea").text("Invalid properties file");
+        }
+    } else {
+        $("main > textarea").text("Invalid fts file");
+    }
+}
+
 // Requests data at /yield to append inside the textarea,
 // if the received status code is 206 (partial content) and response
 // contains some data waits 1000 ms before requesting data again otherwise
@@ -89,4 +113,25 @@ function process_update(wait)
             }
         }
     });
+}
+
+//Loads a file containing the properties to be verified by vmc
+function upload_prop_file(event)
+{
+    if($("#vmc")[0].files[0]) {
+        var file = new FormData();
+        file.append('file', $("#vmc")[0].files[0]);
+        $.ajax({
+            url: '/vmc_upload',
+            data: file,
+            processData: false,
+            contentType: false,
+            type: 'POST',
+            success: function(response) {
+                $("main > textarea").text(response);
+            }
+        });
+    } else {
+        $("main > textarea").text("Model file is missing.");
+    }
 }
