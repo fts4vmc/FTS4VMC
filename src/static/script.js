d75fd027b9a8b407f67e49d26beda01f11e8dee0
Giordano <giordanoscarso@gmail.com>
2020-09-24 17:49:09 +0200

    Added animations for inserting and removing buttons

--- src/static/script.js
+++ src/static/script.js
@@ -2,13 +2,23 @@ $(function(){
     $("aside").on("change", "#fts", alter_title);
     $("aside").on("click", "#load", upload_file);
     $("aside").on("click", "#full", 
-      {url: '/full_analysis', success:timed_update_textarea}, command);
+      {url: '/full_analysis', success:timed_update_textarea, 
+        show:[
+          $("#disambiguate"), $("#fopt"), $("#hdd"), 
+          $("#full"), $("#hdead"), $("#delete"), $("#stop")
+        ]
+      }, command);
+
     $("aside").on("click", "#hdead", 
-      {url: '/hdead_analysis', success:timed_update_textarea}, command);
+      {url: '/hdead_analysis', success:timed_update_textarea, 
+        show:[$("#full"), $("#hdead"), $("#delete"), $("#stop")] 
+      }, command);
+
     $("aside").on("click", "#delete", 
-      {url: '/delete_model', success:update_textarea}, command);
+      {url: '/delete_model', success:update_textarea, show: [$("#fts-label")]}, command);
     $("aside").on("click", "#stop", 
-      {url: '/stop', success:update_textarea}, command);
+      {url: '/stop', success:update_textarea, 
+        show:[$("#full"), $("#hdead"), $("#delete")]}, command);
     $("aside").on("click", "#disambiguate", 
       {url: '/remove_ambiguities', success:update_textarea}, command);
     $("aside").on("click", "#fopt", 
@@ -22,20 +32,29 @@ function alter_title() {
   $("main > h2").text("Analysis of "+name);
   $("main > textarea").text(name+" selected, click load model to"+
     " upload the file");
+  $("#load").slideDown();
 }
 
 //Updates the main's textarea with the response value.
-function update_textarea(response)
+function update_textarea(show, response)
 {
-    $("main > textarea").text(response);
+  $("main > textarea").text(response);
+  if(show) {
+    $(".command").hide();
+    for (element of show) {
+      element.slideDown();
+    }
+  }
 }
 
 //Updates the main's textarea with the response value,
 //and initiates the polling of process output.
-function timed_update_textarea(response)
+function timed_update_textarea(show, response)
 {
     $("main > textarea").text(response);
-    process_update(1000);
+    $(".command").hide();
+    $("#stop").slideDown();
+    process_update(show, 1000);
 }
 
 function upload_file(event)
@@ -51,6 +70,9 @@ function upload_file(event)
             type: 'POST',
             success: function(response) {
                 $("main > textarea").text(response);
+                $("#full").slideDown();
+                $("#hdead").slideDown();
+                $("#delete_model").slideDown();
             }
         });
     } else {
@@ -65,7 +87,9 @@ function command(event)
             url: event.data.url,
             data: {name: $("#fts")[0].files[0].name},
             type: 'POST',
-            success: event.data.success,
+            success: function(response){
+                event.data.success(event.data.show, response);
+            },
             beforeSend: function(response) {
                 $("main > textarea").text("Processing data...");
             }
@@ -82,7 +106,7 @@ function command(event)
 // If the received status code is 200 appends the data inside the textarea.
 // On completed request scrolls to the bottom of the textarea to emulate
 // terminal behaviour.
-function process_update(wait)
+function process_update(show, wait)
 {
     $.ajax({
         url: '/yield', 
@@ -93,10 +117,17 @@ function process_update(wait)
                     wait = 1000;
                 else
                     wait = wait * 2;
-                setTimeout(process_update.bind(null, wait), wait);
+                setTimeout(process_update.bind(null, show, wait), wait);
             },
             200: function(response) {
                 $("main > textarea").append(response);
+                if(show) {
+                    $(".command").hide();
+                    for (element of show) {
+                      element.slideDown();
+                    }
+                }
+                $("#stop").hide();
             }
         },
         complete: function(response) {
