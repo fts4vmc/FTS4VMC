4a86da6181b134459226a975a927e8024c6af177
Giordano <giordanoscarso@gmail.com>
2020-10-30 19:20:58 +0100

    Added test file for graph.py

--- tests/graph_test.py
+++ tests/graph_test.py
@@ -6,7 +6,7 @@ class TestGraph():
     @pytest.fixture
     def graph(self):
         import os
-        source = Graph.from_file(os.path.join('tests', 'test_fts_disambiguato.dot'))
+        source = Graph.from_file(os.path.join('tests', 'dot', 'test_fts_disambiguato.dot'))
         return source
 
     def test_draw_graph(self, graph, tmp_path):
@@ -19,3 +19,23 @@ class TestGraph():
     def test_get_graph_number(self, graph):
         edge, node = graph.get_graph_number()
         assert edge == 5 and node == 5
+
+    def test_file_not_found(self):
+        with pytest.raises(FileNotFoundError):
+            Graph.from_file('nothing')
+
+    def test_wrong_file_type(self):
+        import os
+        with pytest.raises(Exception, match=r"Wrong file format"):
+            Graph.from_file(os.path.join('src', 'static', 'js', 'script.js'))
+
+    def test_wrong_data_type(self):
+        import os
+        with pytest.raises(Exception, match=r"Invalid data"):
+            with open(os.path.join('src','templates', 'main.html')) as source:
+                Graph(source.read())
+
+    def test_permission_error(self, graph):
+        import os
+        with pytest.raises(PermissionError):
+            graph.draw_graph(os.path.join("/","test.svg"))
