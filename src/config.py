b91658d6c89003d2ccb1881e613a575418378913
Giordano <giordanoscarso@gmail.com>
2020-11-16 17:25:41 +0100

    Added config file

---
+++ src/config.py
@@ -0,0 +1,10 @@
+import os
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    UPLOAD_FOLDER = os.path.relpath("uploads")
+    TMP_FOLDER = os.path.join("src",'static','tmp')
+    VMC_LINUX = os.path.relpath('vmc65-linux')
+    VMC_MAC = os.path.relpath('vmc-macos')
+    VMC_WINDOWS = os.path.relpath('vmc-win7.exe')
+    MAX_CONTENT_LENGTH = 2 * 1024 * 1024
